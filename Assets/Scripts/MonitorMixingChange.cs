048311ae1cd05df4a5c83283848c604161d37c88
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2026-04-09 17:33:13 +0200

    Updated color mixing mode switch platform

--- Assets/Scripts/MonitorMixingChange.cs
+++ Assets/Scripts/MonitorMixingChange.cs
@@ -1,23 +1,47 @@
+using System;
 using Seb.Fluid2D.Simulation;
 using System.Collections;
+using PrimeTween;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.VFX;
 
 public class MonitorMixingChange : MonoBehaviour
 {
     [SerializeField] private float colorMixingTimer = 5;
-    [SerializeField] private TextMeshProUGUI timerText;
-    [SerializeField] private Image targetImage;
+    [SerializeField] private TextMeshProUGUI[] timerTexts;
+    [SerializeField] private Image successImage;
+    [SerializeField] private Image previewImage;
+    [SerializeField] private Image fillImage;
     [SerializeField] private Sprite threeColorImage;
     [SerializeField] private Sprite sixColorImage;
+
+    [SerializeField] private Animator _primaryFlareCrateAnimator;
+    [SerializeField] private Animator _secondaryFlareCrateAnimator;
+    [SerializeField] private VisualEffect[] _primaryFlareEffects;
+    [SerializeField] private VisualEffect[] _secondaryFlareEffects;
+    
+    [SerializeField] private float _successImageAnimationDuration = 0.5f;
+    [SerializeField] private float _successImageAnimationScale = 5f;
 
     private float internalColorMixingTimer;
 
     private Collider2D activatingPlayer;
     private Coroutine colorMixingCR;
-
     private FluidSim2D fluidSim;
+
+    private Sequence _successImageSequence;
+
+    private static readonly int FlaresUp = Animator.StringToHash("FlaresUp");
+    private static readonly int FlaresDown = Animator.StringToHash("FlaresDown");
+
+    private void Start()
+    {
+        UpdateTimerTexts(string.Empty);
+        fillImage.fillAmount = 0f;
+        successImage.transform.localScale = Vector3.zero;
+    }
 
     private void OnTriggerEnter2D(Collider2D player)
     {
@@ -28,10 +52,18 @@ public class MonitorMixingChange : MonoBehaviour
 
         Debug.Log(player.gameObject.name);
 
-        if (player.gameObject.name.Contains("Ghost") && player.transform.parent.parent.GetComponent<FluidObstacle>() && player.transform.parent.parent.GetComponent<PlayerDirectionTracker>() && activatingPlayer == null)
+        if (player.gameObject.name.Contains("Ghost") && player.transform.parent.parent.GetComponent<FluidObstacle>() &&
+            player.transform.parent.parent.GetComponent<PlayerDirectionTracker>() && activatingPlayer == null)
         {
             activatingPlayer = player;
             colorMixingCR = StartCoroutine(ColorMixingCR());
+
+            StartPrimaryFlares();
+
+            if (fluidSim.colorMixingActivated)
+            {
+                StartSecondaryFlares();
+            }
         }
     }
 
@@ -44,7 +76,11 @@ public class MonitorMixingChange : MonoBehaviour
             {
                 StopCoroutine(colorMixingCR);
                 colorMixingCR = null;
-                timerText.text = string.Empty;
+                UpdateTimerTexts(string.Empty);
+                fillImage.fillAmount = 0;
+
+                StopPrimaryFlares();
+                StopSecondaryFlares();
             }
         }
     }
@@ -56,11 +92,81 @@ public class MonitorMixingChange : MonoBehaviour
         {
             yield return new WaitForSecondsRealtime(0.1f);
             internalColorMixingTimer -= 0.1f;
-            timerText.text = internalColorMixingTimer.ToString("0.0");
+            UpdateTimerTexts(internalColorMixingTimer.ToString("0"));
+            fillImage.fillAmount = 1 - internalColorMixingTimer / colorMixingTimer;
         }
 
         fluidSim.ToggleColorMixing();
-        targetImage.sprite = fluidSim.colorMixingActivated ? sixColorImage : threeColorImage;
-        timerText.text = string.Empty;
+        successImage.sprite = fluidSim.colorMixingActivated ? threeColorImage : sixColorImage;
+        previewImage.sprite = fluidSim.colorMixingActivated ? sixColorImage : threeColorImage;
+        UpdateTimerTexts(string.Empty);
+        fillImage.fillAmount = 0;
+
+        StopPrimaryFlares();
+        StopSecondaryFlares();
+        AnimateSuccessImage();
+    }
+
+    private void StartPrimaryFlares()
+    {
+        foreach (VisualEffect flare in _primaryFlareEffects)
+        {
+            flare.Play();
+        }
+
+        _primaryFlareCrateAnimator.ResetTrigger(FlaresDown);
+        _primaryFlareCrateAnimator.SetTrigger(FlaresUp);
+    }
+
+    private void StartSecondaryFlares()
+    {
+        foreach (VisualEffect flare in _secondaryFlareEffects)
+        {
+            flare.Play();
+        }
+
+        _secondaryFlareCrateAnimator.ResetTrigger(FlaresDown);
+        _secondaryFlareCrateAnimator.SetTrigger(FlaresUp);
+    }
+
+    private void StopPrimaryFlares()
+    {
+        foreach (VisualEffect flare in _primaryFlareEffects)
+        {
+            flare.Stop();
+        }
+
+        _primaryFlareCrateAnimator.SetTrigger(FlaresDown);
+    }
+
+    private void StopSecondaryFlares()
+    {
+        foreach (VisualEffect flare in _secondaryFlareEffects)
+        {
+            flare.Stop();
+        }
+
+        _secondaryFlareCrateAnimator.SetTrigger(FlaresDown);
+    }
+
+    private void UpdateTimerTexts(string text)
+    {
+        foreach (TextMeshProUGUI timerText in timerTexts)
+        {
+            timerText.text = text;
+        }
+    }
+
+    private void AnimateSuccessImage()
+    {
+        _successImageSequence.Stop();
+        successImage.transform.localScale = Vector3.one;
+        successImage.transform.eulerAngles = Vector3.zero;
+        successImage.color = Color.white;
+        
+        _successImageSequence = Sequence.Create()
+            .Chain(Tween.Scale(successImage.transform, Vector3.one * _successImageAnimationScale, _successImageAnimationDuration).SetEase(Ease.OutCubic))
+            .Group(Tween.Alpha(successImage, 0f, _successImageAnimationDuration).SetEase(Ease.Linear))
+            .Group(Tween.Rotation(successImage.transform, Quaternion.Euler(0, 0, 180), _successImageAnimationDuration).SetEase(Ease.Linear));
     }
 }
